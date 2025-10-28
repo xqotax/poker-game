68653e36d00cdeab8cc37dba11739c755a9294dc
Yurij Skripnik <skripnik.petro@gmail.com>
2025-10-29 00:06:42 +0200

    fix: remove TypeNumber from GameRound

---
+++ src/Infrastructure/Migrations/20251028220553_Remove_TypeNumber_FromRound.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations;
+
+/// <inheritdoc />
+public partial class Remove_TypeNumber_FromRound : Migration
+{
+	/// <inheritdoc />
+	protected override void Up(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.DropColumn(
+			name: "TypeNumber",
+			schema: "poker-game",
+			table: "GameRounds");
+	}
+
+	/// <inheritdoc />
+	protected override void Down(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.AddColumn<long>(
+			name: "TypeNumber",
+			schema: "poker-game",
+			table: "GameRounds",
+			type: "bigint",
+			nullable: false,
+			defaultValue: 0L);
+	}
+}
